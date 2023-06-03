d32ebfd2326bf5b7485ddcc5ba2b20b53c0670f3
DipakDai <dipakkumal673@gmail.com>
2023-06-03 23:10:15 +0545

    complete dashboardCard v1.1.1

--- admin-dashboard/src/components/pages/Dashboard.jsx
+++ admin-dashboard/src/components/pages/Dashboard.jsx
@@ -1,17 +1,37 @@
 import React from "react";
-import {  Row } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import styled from "styled-components";
 import DashboardCard from "../scene/DashboardCard";
-import GroupsIcon from '@mui/icons-material/Groups';
-
+import GroupsIcon from "@mui/icons-material/Groups";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import MoneyOffIcon from "@mui/icons-material/MoneyOff";
 const Dashboard = () => {
+  
   return (
     <div className="main__content container">
       <Container>
         <Row className="gap-4">
-          <DashboardCard heading ="Customer"  num={203} link="/customer" />
-          <DashboardCard heading ="Order"  num={140} link="/customer" />
-          <DashboardCard heading ="Profit"  num={50343} link="/customer" />
+          <DashboardCard
+            heading="Customer"
+            num={203}
+            link="/customer"
+            icon={<GroupsIcon />}
+            color="text-danger"
+          />
+          <DashboardCard
+            heading="Order"
+            num={140}
+            link="/customer"
+            icon={<ShoppingBasketIcon />}
+            color="text-dark"
+          />
+          <DashboardCard
+            heading="Profit"
+            num={"$50343"}
+            link="/customer"
+            icon={<MoneyOffIcon />}
+            color="text-primary"
+          />
         </Row>
       </Container>
     </div>
--- admin-dashboard/src/components/scene/DashboardCard.jsx
+++ admin-dashboard/src/components/scene/DashboardCard.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Col } from "react-bootstrap";
-const DashboardCard = ({heading,num,link}) => {
+const DashboardCard = ({heading,num,link,icon,color}) => {
+    const cardStyle = {borderLeft:'5px solid',borderColor:color  };    
   return (
-      <Col md="3" className="shadow-lg py-2">
+      <Col md="3" className="shadow-lg py-2" 
+       style={cardStyle}
+      >
         <div className="customers">
           <div className="row">
             <div className="col-md-6">
               <h6 style={{fontWeight:"800"}} className="text-dark">{heading}</h6>
             </div>
             <div className="col-md-6">
-              <h6>
+              <h6 className={color}>
+                    {icon}
               </h6>
             </div>
           </div>
