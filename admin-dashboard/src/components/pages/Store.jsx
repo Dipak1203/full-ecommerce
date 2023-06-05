a33dba51b6eed2110dbaa0fc97dacbb307448394
DipakDai <dipakkumal673@gmail.com>
2023-06-05 23:13:41 +0545

    store v1.0.1

--- admin-dashboard/src/components/pages/Store.jsx
+++ admin-dashboard/src/components/pages/Store.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import * as React from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { useDemoData } from "@mui/x-data-grid-generator";
+import { Col, Row } from "react-bootstrap";
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import faker from "faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const barData = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
 const Store = () => {
+  const { data } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: 100,
+    maxColumns: 6,
+  });
   return (
-    <div className='main__content'>Store</div>
-  )
-}
+    <div className="main__content container">
+      <Row className="mt-4">
+        <Col md="6">
+          <h2 style={{fontWeight:'800',borderBottom:'1px solid #000'}} className="text-dark">Store</h2>
+          <Bar options={options} data={barData} />
+        </Col>
+        <Col md="6" style={{ height: 400, width: "" }}>
+          <DataGrid
+            {...data}
+            slots={{
+              toolbar: GridToolbar,
+            }}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-export default Store
+export default Store;
