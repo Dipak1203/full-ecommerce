8702cf2d5c112bef717ef54933783a7c6a8751c7
DipakDai <dipakkumal673@gmail.com>
2023-06-18 22:43:59 +0545

    v2 complete

--- admin-dashboard/src/App.jsx
+++ admin-dashboard/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route,useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Dashboard from "./components/pages/Dashboard";
 import Login from "./components/auth/Login";
 import Sidebar from "./components/Sidebar";
@@ -12,41 +14,51 @@ import Messages from "./components/pages/Messages";
 import HelpCenter from "./components/pages/HelpCenter";
 import Setting from "./components/pages/Setting";
 import Logout from "./components/pages/Logout";
-import "bootstrap/dist/css/bootstrap.min.css";
 import ProductForm from "./components/forms/ProductForm";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const App = () => {
   return (
     <BrowserRouter>
-    <AppContent />
+      <AppContent />
     </BrowserRouter>
   );
 };
 
 export default App;
 
-
-const AppContent = () =>{
+const AppContent = () => {
   const location = useLocation();
+  const isLoggedIn = !!localStorage.getItem("token");
 
   // Check if the current route is the Login route
   const isLoginPage = location.pathname === "/";
+
+  const PrivateRoute = ({ element, path }) => {
+    if (!isLoggedIn && !isLoginPage) {
+      return <Navigate to="/" />;
+    }
+    return element;
+  };
+
   return (
     <>
-    {!isLoginPage && <Sidebar />}
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/statistics" element={<Statistics />} />
-      <Route path="/store" element={<Store />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/product/create" element={<ProductForm />} />
-      <Route path="/finances" element={<Finances />} />
-      <Route path="/invoices" element={<Invoices />} />
-      <Route path="/message" element={<Messages />} />
-      <Route path="/helpCenter" element={<HelpCenter />} />
-      <Route path="/setting" element={<Setting />} />
-      <Route path="/logout" element={<Logout />} />
-    </Routes>
-  </>
-  )
-}
+      {!isLoginPage && <Sidebar />}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+        <Route path="/statistics" element={<PrivateRoute element={<Statistics />} />} />
+        <Route path="/store" element={<PrivateRoute element={<Store />} />} />
+        <Route path="/products" element={<PrivateRoute element={<Products />} />} />
+        <Route path="/product/create" element={<PrivateRoute element={<ProductForm />} />} />
+        <Route path="/finances" element={<PrivateRoute element={<Finances />} />} />
+        <Route path="/invoices" element={<PrivateRoute element={<Invoices />} />} />
+        <Route path="/message" element={<PrivateRoute element={<Messages />} />} />
+        <Route path="/helpCenter" element={<PrivateRoute element={<HelpCenter />} />} />
+        <Route path="/setting" element={<PrivateRoute element={<Setting />} />} />
+        <Route path="/logout" element={<PrivateRoute element={<Logout />} />} />
+      </Routes>
+      <ToastContainer />
+    </>
+  );
+};
--- admin-dashboard/src/components/auth/Login.jsx
+++ admin-dashboard/src/components/auth/Login.jsx
@@ -1,85 +1,139 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:8000/login/AuthAdminActivity', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        const { token } = await response.json();
+        // Set token in localStorage
+        localStorage.setItem('token', token);
+        // Redirect to dashboard component
+        window.location.href = '/dashboard';
+      } else {
+        const { message } = await response.json();
+        // Display error toast
+        toast.error(message);
+      }
+    } catch (error) {
+      // Handle network or server error
+      console.log('An error occurred:', error);
+    }
+  };
+
   return (
-   <Container>
-     <div className="login">
-      <div className="login_box shadow-lg">
-        <h3>Hello !</h3>
-        <p>Sign into your account</p>
-        <input type="email" className="input" placeholder="example@gmail.com" />
-        <input type="password" className="input" placeholder="your password" />
-        <button className="button">Submit</button>
-        <div className="login_content">
-          <div>
-            <input type="checkbox" />
-            <span> Remember me</span>
+    <Container>
+      <div className="login">
+        <div className="login_box shadow-lg">
+          <h3>Hello!</h3>
+          <p>Sign into your account</p>
+          <form onSubmit={handleLogin}>
+            <input
+              type="text"
+              className="input"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="password"
+              className="input"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit" className="button">
+              Submit
+            </button>
+          </form>
+          <div className="login_content">
+            <div>
+              <input type="checkbox" />
+              <span>Remember me</span>
+            </div>
+            <a href="/">Forgot password?</a>
           </div>
-          <a href="">Forgot password?</a>
         </div>
       </div>
-    </div>
-   </Container>
+      <ToastContainer />
+    </Container>
   );
 };
 
-
 const Container = styled.section`
-  
-
-.login{
+  .login {
     height: 100vh;
-    display:flex;
-    justify-content:center;
-    align-items:center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     background-color: #e0dede;
     border: 2px solid #00ab55;
-}
+  }
 
-.login_box{
+  .login_box {
     width: 600px;
     height: 300px;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    flex-direction: column;
     align-items: center;
     background-color: #efefef;
     border-radius: 10px;
-}
+  }
 
-.login_box h1,p,h3,h2,a,h4{
+  .login_box h3,
+  .login_box p,
+  .login_box h1,
+  .login_box h2,
+  .login_box a,
+  .login_box h4 {
     color: #000 !important;
     font-weight: 900 !important;
-}
+  }
 
-.input{
+  .input {
     padding: 7px 27px;
     margin-bottom: 10px;
     border-bottom: 2px solid;
     border: none;
     border: 2px solid #00ab55;
     border-radius: 4px;
-}
-.button{
+  }
+
+  .button {
     background: #3cc3be;
     padding: 10px 86px;
     cursor: pointer;
     border-radius: 50px;
     font-weight: 700;
-}
+  }
 
-.button:hover{
+  .button:hover {
     background: #2fd397 !important;
-}
+  }
 
-.login_content{
+  .login_content {
     width: 300px;
-    display:flex;
-    justify-content:space-between;
-    align-items:center;
-}
-`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
 
 export default Login;
