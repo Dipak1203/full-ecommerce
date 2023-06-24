da71fe1fc782ac349c16145d3e149a77ebbda125
DipakDai <dipakkumal673@gmail.com>
2023-06-24 15:57:49 +0545

    hero section added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import { styled } from "styled-components";
 import CarouselComponent from "../components/home/CarouselComponent";
+import {Col, Container, Row} from 'react-bootstrap';
 function Home() {
   return (
-    <Content>
-      <div>
-       <CarouselComponent />
-      </div>
-    </Content>
+    <Container fluid>
+      <Row>
+        <Col md="3">
+           <img src="https://media0.giphy.com/media/o1IlGxfDSWhyjduK0c/200w.gif?cid=6c09b952v4mdc2zshm77ingdvpj9jyklx4g6op3t29b92pfb&ep=v1_gifs_search&rid=200w.gif&ct=g" />
+           <img src="https://media0.giphy.com/media/o1IlGxfDSWhyjduK0c/200w.gif?cid=6c09b952v4mdc2zshm77ingdvpj9jyklx4g6op3t29b92pfb&ep=v1_gifs_search&rid=200w.gif&ct=g" />
+        </Col>
+        <Col md="6">
+        <CarouselComponent />
+        </Col>
+        <Col md="3">
+           <img src="https://media0.giphy.com/media/o1IlGxfDSWhyjduK0c/200w.gif?cid=6c09b952v4mdc2zshm77ingdvpj9jyklx4g6op3t29b92pfb&ep=v1_gifs_search&rid=200w.gif&ct=g" />
+           <img src="https://media0.giphy.com/media/o1IlGxfDSWhyjduK0c/200w.gif?cid=6c09b952v4mdc2zshm77ingdvpj9jyklx4g6op3t29b92pfb&ep=v1_gifs_search&rid=200w.gif&ct=g" />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
