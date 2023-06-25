98786b7ef4fb03bc7222edff6874f4e5d6f167e6
DipakDai <dipakkumal673@gmail.com>
2023-06-25 20:30:47 +0545

    btn hover added

--- client/src/scenes/Button.jsx
+++ client/src/scenes/Button.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import { styled } from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
-const Button = ({title}) => {
-  return (
-    <Btn>{title}</Btn>
-  )
-}
+const Button = ({ title }) => {
+  return <Btn>{title}</Btn>;
+};
 
 const Btn = styled.button`
-    border: none;
-    outline: none;
-    background: #000;
-    color:#fff;
-    padding:.5rem 1rem ;
-    font-size:15px;
-    border-radius: 4px;
-`
+  border: none;
+  outline: none;
+  background: #000;
+  color: #fff;
+  padding: 0.5rem 1rem;
+  font-size: 15px;
+  border-radius: 4px;
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
+  transition: all 0.3s ease;
 
-export default Button
+  &:hover {
+    transform: scale(1.1);
+    transition: all 0.3s ease;
+  }
+
+  &:hover:after {
+    height: 100%;
+  }
+`;
+
+export default Button;
